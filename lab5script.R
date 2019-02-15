b7a813f711d020da4e54688cd55f2059e226305e
johnmasonharris <john.mason.harris@gmail.com>
2019-02-15 15:12:04 -0600

    AK and Peru date and coordinates ready for arc

--- lab5script.R
+++ lab5script.R
@@ -11,3 +11,20 @@ install.packages("devtools")
 AK = read.csv(file = "Alaska1.csv")
 GOM = read.csv(file = "GOM1.csv")
 Peru = read.csv(file = "Peru1.csv")
+
+#Alaska
+#did in excel - good to go
+
+#Peru
+View(Peru)
+#make lon decimal form
+Peru$lon = gsub('°', ' ', Peru$lon)
+Peru$lon = gsub(' ', '.', Peru$lon)
+# now drop letter at end
+Peru$lon = gsub('W', '', Peru$lon)
+#repeat for lat
+Peru$lat = gsub('°', ' ', Peru$lat)
+Peru$lat = gsub(' ', '.', Peru$lat)
+Peru$lat = gsub('S', '', Peru$lat)
+
+#Gulf of Mexico
