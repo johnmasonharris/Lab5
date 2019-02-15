8369cceed84f68ce65b8cc130bc4b54a5b44e08a
johnmasonharris <john.mason.harris@gmail.com>
2019-02-15 14:14:06 -0600

    read in csvs to edit time and date

--- lab5script.R
+++ lab5script.R
@@ -1,1 +1,13 @@
 #alright lets try this again
+
+library(plyr)
+library(stringr)
+library(pastecs)
+library(oce)
+library(lubridate)
+install.packages("devtools")
+
+#read in files
+AK = read.csv(file = "Alaska1.csv")
+GOM = read.csv(file = "GOM1.csv")
+Peru = read.csv(file = "Peru1.csv")
