0900553e8053d4b1793baab34fe055aca2f67c28
johnmasonharris <john.mason.harris@gmail.com>
2019-02-18 10:20:41 -0600

    exported tab-delimited text files

--- lab5script.R
+++ lab5script.R
@@ -5,8 +5,7 @@ library(stringr)
 library(pastecs)
 library(oce)
 library(lubridate)
-install.packages("devtools")
-
+library(devtools)
 #read in files
 AK = read.csv(file = "Alaska1.csv")
 GOM = read.csv(file = "GOM1.csv")
@@ -26,5 +25,31 @@ Peru$lon = gsub('W', '', Peru$lon)
 Peru$lat = gsub('°', ' ', Peru$lat)
 Peru$lat = gsub(' ', '.', Peru$lat)
 Peru$lat = gsub('S', '', Peru$lat)
+# reformat dates
+Peru$date = as.Date(Peru$date, "%m/%d/%Y")
+str(Peru$date)
 
-#Gulf of Mexico
+#Gulf of Mexico 
+View(GOM)
+# lat and lon are good so here is date
+#add zeros to month and days with one number
+mm = str_sub(GOM$Month, 1, 2)
+dd = str_sub(GOM$Day, 1, 2)
+yyyy = (GOM$Year)
+
+GOM$date = as.Date(with(GOM, paste(mm, dd, yyyy, sep="/")), "%m/%d/%Y")
+
+#both peru and GOM show up in r tables as year-month-day despite the order in the %m/%d/%Y function...lets see if arc cares
+
+
+#save over all existing csvs
+write.csv(AK, file = "Alaska1.csv")
+write.csv(Peru, file = "Peru1.csv")
+write.csv(GOM, file = "GOM1.csv")
+
+# now export as tab-delimited text files
+write.table(AK, "Alaska1.txt", append = FALSE, sep = " ", dec = ".", row.names = TRUE, col.names = TRUE)
+write.table(Peru, "Peru1.txt", append = FALSE, sep = " ", dec = ".", row.names = TRUE, col.names = TRUE)
+write.table(GOM, "GOM1.txt", append = FALSE, sep = " ", dec = ".", row.names = TRUE, col.names = TRUE)
+
+
